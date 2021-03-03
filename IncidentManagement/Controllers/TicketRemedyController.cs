e7ec426d1edf05b855925036ce05f6545a86d84a
VerserGitRepository <basanagouda.patil@verser.com.au>
2021-03-03 16:37:19 +1100

    new changes

---
+++ IncidentManagement/Controllers/TicketRemedyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IncidentManagement.Controllers
+{
+    public class TicketRemedyController : Controller
+    {
+        // GET: TicketRemedy
+        public ActionResult Index()
+        {
+            if (Session["UserName"] !=null && Session["Administrator"] != null)
+            {
+                return View();
+            }
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
